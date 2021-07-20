28297fce780a8a9109eced93c9e62b47a408dd8c
Sandor Kovacs <sandorkovacs84@gmail.com>
2021-07-20 19:26:40 +0300

    add watcher

--- src/api/UserRepositories.js
+++ src/api/UserRepositories.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const  fetchUsers = () => axios.get('https://jsonplaceholder.typicode.com/users')
-                            .then(response => response.data);
+const  fetchUsers = () => axios.get('https://jsonplaceholder.typicode.com/users');
+                            // .then(response => response.data);
 
 export  {
     fetchUsers,
--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>User Component</h1>
+
+        <counter-component></counter-component>
+
+        <input type="number" v-model="a">
+
         <!-- {{ users }} -->
         <ul>
             <li v-for="user in users" :key="user.id">
@@ -12,42 +17,39 @@
 
 <script>
 
-// import axios from 'axios'
-import { fetchUsers } from '@/api/UserRepositories'
-import { ref } from 'vue'
+import axios from 'axios'
+// import { fetchUsers } from '@/api/UserRepositories'
+import { ref, onMounted, watch } from 'vue'
+
+import CounterComponent from '@/components/CounterComponent';
 
 export default {
-    data() {
-        return {
-            // users: [{'name': 'Sandor', id: 777}]
-        }
+    components: {
+        CounterComponent,
     },
 
     setup() {
+        const a = ref(1);
+
         const users = ref([]); 
 
         const getUserRepositories = async () => {
-            // users.value = await axios.get('https://jsonplaceholder.typicode.com/users') ;
-            users.value = await fetchUsers() ;
+            let res = await axios.get('https://jsonplaceholder.typicode.com/users') ;
+            // let res = await fetchUsers();
+            users.value = res.data 
         }
 
+        onMounted(getUserRepositories);
+
+        watch(a, getUserRepositories);
+
         return {
+            a,
             users,
             getUserRepositories
         }
     },
 
-    mounted() {
-        this.getUserRepositories();
-    },
-
-    methods: {
-        // fetchUsers() {
-        //     axios.get('https://jsonplaceholder.typicode.com/users').then( response => {
-        //         this.users = response.data;
-        //     });
-        // }
-    }
  
 
 }
